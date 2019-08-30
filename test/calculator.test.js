577a075e965efbbcd3b8d2c6224eee6d13965275
Hendrik Peter van der Meulen <peter@picoreclame.nl>
2019-08-30 16:00:17 +0200

    fix

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -4,7 +4,7 @@ describe('calculator', () => {
   describe('add', () => {
     it('adds two numbers', () => {
       const result = calculator.add(2, 3);
-      expect(result).toBe(4);
+      expect(result).toBe(5);
     });
   });
 });
